f57076ecccff296ecad00ca8b779016d95e0f4b3
Zachary Kipping <zjkipping@hotmail.com>
2018-03-01 13:43:26 -0600

    Created XCode Project and updated .gitignore

---
+++ StoryOrganizer/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  StoryOrganizer
+//
+//  Created by Zachary Kipping on 3/1/18.
+//  Copyright © 2018 Zachary Kipping. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
